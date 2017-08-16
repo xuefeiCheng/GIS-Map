e71fe550d41a1b14f3aae2bea3d9b9116a3c01cd
程雪飞 <1805711033@qq.com>
2017-08-16 08:37:16 +0800

    在map中引用自定义controller

--- dist/locate.js
+++ dist/locate.js
@@ -177,6 +177,44 @@ L.Control.Locate = L.Control.extend({
         return container;
     }
 });
+//自定义 controler
+L.Control.Legend = L.Control.extend({
+    options: {
+        position: 'topright' //初始位置
+
+    },
+    initialize: function (options) {
+        L.Util.extend(this.options, options);
+
+    },
+    onAdd: function (map) {
+       //创建一个class为leaflet-control-clegend的div
+        this._container = L.DomUtil.create('div', 'leaflet-control-clegend');
+       //创建一个图片要素
+        var legendimg = document.createElement('img');
+        legendimg.id = 'leaflet-control-clegend';
+        legendimg.type = 'img';
+        legendimg.src = "dist/images/markers-matte.png";
+        this._legendimg = legendimg;
+        //创建一个关闭控件的按钮
+        var closebutton = document.createElement('a');
+        closebutton.id = 'leaflet-control-geosearch-close';
+        closebutton.className = 'glyphicon glyphicon-remove';
+        this._closebutton = closebutton;
+        
+        this._container.appendChild(this._closebutton);
+        this._container.appendChild(this._legendimg);
+        //注册关闭事件
+        L.DomEvent.addListener(this._closebutton, 'click', this._onCloseControl, this);
+        
+        return this._container;
+    },
+    _onCloseControl: function () {
+        this._map.options.Legend = false;
+        this.removeFrom(this._map);
+
+    }
+    });
 
 L.Map.addInitHook(function () {
     if (this.options.locateControl) {
@@ -187,4 +225,7 @@ L.Map.addInitHook(function () {
 
 L.control.locate = function (options) {
     return new L.Control.Locate(options);
+};
+L.control.Legend =function(options){
+	return new L.Control.Legend(options);
 };
